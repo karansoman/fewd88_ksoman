281e4d3efd45267fd9e27c039ef9b9ae8e21d659
karansoman <kas721@nyu.edu>
2016-06-28 18:30:31 -0400

    Updates
    Updates

---
+++ Week04/class07/first-javascript/script.js
@@ -0,0 +1,43 @@
+document.write('writing to the document, like a boss');
+document.write((10*10)+'<br/>');
+document.write(100/10);
+document.write(50-17);
+document.write(50+17);
+document.write("Karan Soman")
+
+var fname = "Karan";
+var lname = "Soman";
+
+document.write(fname + " " + lname)
+
+console.log("hello");
+
+var num1= 10*10;
+var num2= 100/10;
+var num3= 50-17;
+var num4=50+17;
+
+
+document.write(num1 + '<br/>');
+document.write(num2);
+document.write(num3);
+
+
+// // function definition 
+function doSomething(){
+	alert('this is an alert.this is something')};
+
+doSomething();
+
+
+document.getElementById('greeting').style.color="red";
+
+
+var mypara = document.getElementById('mypara');
+mypara.style.color="green";
+mypara.onclick = doSomething;
+
+
+
+
+
