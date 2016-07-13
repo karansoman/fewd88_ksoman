d9b576960c96073a7ad4d05efd3a0798e4c1be21
karansoman <kas721@nyu.edu>
2016-07-13 18:32:27 -0400

    Class
    Updates

--- Week06/class10/jquery-review/js/review.js
+++ Week06/class10/jquery-review/js/review.js
@@ -1,7 +1,20 @@
-// 1. Use document.getElementById() and attach an .onclick event handler to #clickme
+// 1. Use document.getElementById() and attach an 
+//.onclick event handler to #clickme
+$('#one').click(){.css('background-color','blue').text('hey whats up');
+}
+
 // 2. Assign this event handler to a function named tellFortune
+
 // 3. The function ellFortune should:
-// - A) store the values from the four inputs (#hometown, #partner, #favNum, #jobTitle) in variables
+// - A) store the values from the four inputs 
+//(#hometown, #partner, #favNum, #jobTitle) in variables
+
+
+
 // - B) concatenate these variables into the sentence:
-// - "You will be a " + jobTitle + " living in " + hometown + " married to " + partner + " with " + favNum + " kids."
+// - "You will be a " + jobTitle + " living in " 
+//+ hometown + " married to " + partner + " with " + favNum + " kids."
+
+
+
 // - C) output this string to #output
